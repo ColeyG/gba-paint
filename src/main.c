b9256f9511c091b86d8957fdbf6cbc63c4e37be6
ColeyG <cjgeerts@gmail.com>
2022-02-16 13:52:22 -0500

    Breaking the Compiler??

--- src/main.c
+++ src/main.c
@@ -1,13 +1,84 @@
 #include <time.h>
 #include <stdlib.h>
 
+#define MEM_IO 0x04000000
+#define REG_DISPLAY (*((volatile uint32 *)(MEM_IO)))
+#define REG_DISPLAY_VCOUNT (*((volatile uint32 *)(MEM_IO + 0x0006)))
+#define REG_KEY_INPUT (*((volatile uint32 *)(MEM_IO + 0x0130)))
+
+#define KEY_A 0x0001
+#define KEY_B 0x0002
+
+#define KEY_SELECT 0x0004
+#define KEY_START 0x0008
+
+#define KEY_RIGHT 0x0050
+#define KEY_LEFT 0x0060
+#define KEY_UP 0x0070
+#define KEY_DOWN 0x0080
+
+// #define KEY_R 0x0100
+// #define KEY_L 0x0100
+
+#define KEY_ANY 0x03FF
+
+typedef unsigned int uint32;
+
+// Gross Globals Area
+int x = 10;
+int y = 10;
+int colour = 0x001F;
+
 void place(volatile unsigned short vram[], int x, int y, int color)
 {
   vram[y * 240 + x] = color;
 }
 
-void initialize()
+void update(volatile unsigned short vram[], int keyStates)
 {
+  if (keyStates & KEY_LEFT)
+  {
+    x--;
+  }
+
+  if (keyStates & KEY_RIGHT)
+  {
+    x++;
+  }
+
+  if (keyStates & KEY_UP)
+  {
+    y--;
+  }
+
+  if (keyStates & KEY_DOWN)
+  {
+    y++;
+  }
+
+  if (x > 240)
+  {
+    x = 240;
+  }
+
+  if (x < 0)
+  {
+    x = 0;
+  }
+
+  if (y > 160)
+  {
+    y = 160;
+  }
+
+  if (y < 0)
+  {
+    y = 0;
+  }
+
+  place(vram, x, y, colour);
+
+  // colour++;
 }
 
 int main(void)
@@ -19,26 +90,22 @@ int main(void)
 
   // Write pixel colours into VRAM
   volatile unsigned short *vram = (unsigned short *)0x06000000;
-  vram[80 * 240 + 115] = 0x001F; // X = 115, Y = 80, C = 000000000011111 = R
-  vram[80 * 240 + 120] = 0x03E0; // X = 120, Y = 80, C = 000001111100000 = G
-  vram[80 * 240 + 125] = 0x7C00; // X = 125, Y = 80, C = 111110000000000 = B
+  uint32 keyStates = 0;
 
   srand(time(NULL));
-
-  int col1 = 0x001F;
-  int col2 = 0x03E0;
-  int col3 = 0x7C00;
 
   // Wait forever
   while (1)
   {
-    place(vram, rand() % 240, rand() % 160, col1);
-    place(vram, rand() % 240, rand() % 160, col2);
-    place(vram, rand() % 240, rand() % 160, col3);
 
-    col1++;
-    col2++;
-    col3++;
+    while (REG_DISPLAY_VCOUNT >= 160)
+      ;
+    while (REG_DISPLAY_VCOUNT < 160)
+    {
+      keyStates = ~REG_KEY_INPUT & KEY_ANY;
+
+      update(vram, keyStates);
+    }
   }
 
   return 0;
