45271b72eefbadfe2db0721a6690000292d54064
ColeyG <cjgeerts@gmail.com>
2022-05-15 17:19:15 -0400

    Adding Image Transfer script final

--- ops/scripts/image-transfer.js
+++ ops/scripts/image-transfer.js
@@ -15,8 +15,25 @@ const includesAny = (string, array) => {
   return false;
 }
 
-const transferImageFile = (file) => {
-  Jimp.read(`images/raw/${file}`).then((image) => {
+// Checks if the colour is in the map already and returns the index if it finds it
+const matchColours = (array, colour) => {
+  let result = undefined;
+
+  array.forEach((item, index) => {
+    if (
+      item.r === colour.r &&
+      item.g === colour.g &&
+      item.b === colour.b
+    ) {
+      result = index;
+    }
+  });
+
+  return result;
+}
+
+const transferImageFile = async (file) => {
+  Jimp.read(`images/raw/${file}`).then(async (image) => {
     console.log(`Transferring ${file}...`);
 
     const width = image.bitmap.width;
@@ -24,21 +41,52 @@ const transferImageFile = (file) => {
 
     console.log(`w: ${width} h: ${height}`);
 
+    // Image Globals...
+
+    const colourMap = [];
+    const imageArray = [];
+
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
-        // console.log(`x: ${x} y: ${y}`);
-        console.log(Jimp.intToRGBA(image.getPixelColor(x, y)));
+        const pixelColour = Jimp.intToRGBA(image.getPixelColor(x, y));
 
         // Add the colour to the colour map if it is not already there...
 
-        // Add an index to the image array bound to the colour map...
+        let result = matchColours(colourMap, pixelColour);
 
-        // Make each rgb value 5 bit instead of 8 bit integers...
-        // Divide by 256 and multiply by 32 and encode as hex...
+        if (result === undefined) {
+          imageArray.push(colourMap.length);
 
-        // Write the file to the processed directory...
+          colourMap.push(pixelColour);
+        } else {
+          imageArray.push(result);
+        }
       }
     }
+
+    const fixedColourMap = [];
+
+    colourMap.forEach((colour) => {
+      const redBits = Math.round(colour.r / 256 * 32);
+      const greenBits = Math.round(colour.g / 256 * 32);
+      const blueBits = Math.round(colour.b / 256 * 32);
+
+      const hex = parseInt(`${redBits.toString(2)}${greenBits.toString(2)}${blueBits.toString(2)}`, 2).toString(16).toUpperCase();
+
+      fixedColourMap.push(`0x${hex}`);
+    });
+
+    const fileName = file.replace('.png', '.c');
+    const imageName = file.split('.')[0];
+
+    // Write the file to the processed directory...
+    await fs.writeFile(
+      `images/processed/${fileName}`,
+      `int ${imageName}ColorMap[] = {${fixedColourMap}};
+int ${imageName}[] = {${imageArray}};
+int ${imageName}Size = ${imageArray.length};
+int ${imageName}Width = ${width};\n`
+    );
   });
 }
 
